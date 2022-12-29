501900ea2a11a82df6b1c9bac86fefe5ce13208f
Khaled M. ALaa <khaled.mohamed@swvl.com>
2022-12-30 00:41:18 +0200

    feat: modify 404 page view

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,7 +1,35 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Header, Image } from "semantic-ui-react";
+import MainHeader from "../../components/Header";
 
 const NotFound: FC = () => {
-  return <div>404</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <MainHeader />
+      <div
+        style={{
+          marginTop: "5rem",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexFlow: "column",
+        }}
+      >
+        <Image
+          alt="Logo"
+          src="/assets/images/404.PNG"
+          style={{ width: "auto", height: "250px" }}
+        />
+        <Header color="orange" as={"h2"}>PAGE NOT FOUND</Header>
+        <Button color="orange" onClick={() => navigate("/")}>
+          Go Home
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
